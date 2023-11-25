c4b6be064f7b8c23bded1547341753b1688df23d
Jorge Navarrete <giomaussi@hotmail.com>
2023-11-24 22:33:18 -0500

    creaciòn de oreo archiivo para ejecutar sacn-imagenes2

--- leeguarda.py
+++ leeguarda.py
@@ -6,6 +6,14 @@ import json
 
 #ruta = "/home/usuario/Downloads/images.jpeg"
 def obtiene_atributos_imagen(ruta):
+    """Funciòn para leer los atributos de una imagen
+
+    Args:
+        ruta (string): ruta de las imagenes
+
+    Returns:
+        dictionary: atributos de iamgen dentro de un diccionario
+    """    
     try:
         with Image.open(ruta) as img:
             ancho, alto = img.size
@@ -13,20 +21,27 @@ def obtiene_atributos_imagen(ruta):
             im = img.im
             format = img.format
             formatd = img.format_description
-            info = img.info
-            palette = img.palette
+            #info = img.info
+            #palette = img.palette
             
             #print({'nombre': os.path.basename(ruta), 'ancho': ancho, 'alto': alto, 'modo': modo, 'im' : im, 'format' : format, 'formatd': formatd, 'info': info, 'palette' : palette})
-            return({'ruta': ruta, 'nombre': os.path.basename(ruta), 'ancho': ancho, 'alto': alto, 'modo': modo, 'im' : im, 'format' : format, 'formatd': formatd, 'info': info, 'palette' : palette})
+            #return{'ruta': ruta, 'nombre': os.path.basename(ruta), 'ancho': ancho, 'alto': alto, 'modo': modo, 'im' : im, 'format' : format, 'formatd': formatd, 'info': info, 'palette' : palette}
+            return{'ruta': ruta, 'nombre': os.path.basename(ruta), 'ancho': ancho, 'alto': alto, 'modo': modo, 'im' : im, 'format' : format, 'formatd': formatd}
     except Exception as ex:
         return {'nombre': os.path.basename(ruta), 'error': str(ex)}
     
 
 def guarda_json(datos, nombre_archivo):
+    """Guarda de datos de imagen en un archvio json
+
+    Args:
+        datos (_type_): datos para el json
+        nombre_archivo (_type_): Nombre de archivo json
+    """    
     try:
         with open(nombre_archivo,'w') as archivo:
             json.dump(datos, archivo, indent=2)
-        print(f"Satos cuardafod en {nombre_archivo}")
+        print(f"Datos guardados en {nombre_archivo}")
     except Exception as e:
         print(f"Error en guardar jcon: {e}")
             
---
+++ scan-imagenes2.py
@@ -0,0 +1,56 @@
+import os
+import threading
+from PIL import Image
+import json
+import leeguarda
+"""
+def obtener_atributos_imagen(ruta_imagen):
+    try:
+        with Image.open(ruta_imagen) as img:
+            ancho, alto = img.size
+            modo = img.mode
+            return {"nombre": os.path.basename(ruta_imagen), "ruta": ruta_imagen, "ancho": ancho, "alto": alto, "modo": modo}
+    except Exception as e:
+        print(f"No se pudo procesar la imagen {ruta_imagen}. Error: {str(e)}")
+        return None
+"""
+
+
+def procesar_carpeta(carpeta, resultados):
+    for nombre_archivo in os.listdir(carpeta):
+        ruta_archivo = os.path.join(carpeta, nombre_archivo)
+        if os.path.isfile(ruta_archivo): #and nombre_archivo.lower().endswith(('.jpg', '.jpeg', '.png')):
+            atributos = leeguarda.obtiene_atributos_imagen(ruta_archivo)
+            if atributos:
+                resultados.append(atributos)
+                print(ruta_archivo)
+        elif os.path.isdir(ruta_archivo):
+            procesar_carpeta(ruta_archivo, resultados)
+"""
+def guardar_en_json(resultados, nombre_archivo):
+    with open(nombre_archivo, 'w') as archivo_json:
+        json.dump(resultados, archivo_json, indent=4)
+"""
+def procesar_carpeta_principal(carpeta_principal, nombre_archivo_json):
+    resultados = []
+    hilos = []
+
+    for carpeta, _, _ in os.walk(carpeta_principal):
+        hilo = threading.Thread(target=procesar_carpeta, args=(carpeta, resultados))
+        hilos.append(hilo)
+        hilo.start()
+
+    for hilo in hilos:
+        hilo.join()
+
+    leeguarda.guarda_json(resultados, nombre_archivo_json)
+
+if __name__ == "__main__":
+    # Carpeta principal que quieres procesar
+    carpeta_principal = "/home/usuario/Downloads/"
+
+    # Nombre del archivo JSON de salida
+    nombre_archivo_json = "resultados.json"
+
+    procesar_carpeta_principal(carpeta_principal, nombre_archivo_json)
+
