58da28e8ab6052eea1fa7e774c6ad24e93e1cb30
Jorge Navarrete <giomaussi@hotmail.com>
2023-11-25 00:14:17 -0500

    cambios menores

--- scan-imagenes2.py
+++ scan-imagenes2.py
@@ -1,19 +1,8 @@
 import os
 import threading
-#from PIL import Image
-#import json
+
 import leeguarda
-"""
-def obtener_atributos_imagen(ruta_imagen):
-    try:
-        with Image.open(ruta_imagen) as img:
-            ancho, alto = img.size
-            modo = img.mode
-            return {"nombre": os.path.basename(ruta_imagen), "ruta": ruta_imagen, "ancho": ancho, "alto": alto, "modo": modo}
-    except Exception as e:
-        print(f"No se pudo procesar la imagen {ruta_imagen}. Error: {str(e)}")
-        return None
-"""
+
 
 
 def procesar_carpeta(carpeta, resultados):
@@ -26,11 +15,7 @@ def procesar_carpeta(carpeta, resultados):
                # print(ruta_archivo)
         elif os.path.isdir(ruta_archivo):
             procesar_carpeta(ruta_archivo, resultados)
-"""
-def guardar_en_json(resultados, nombre_archivo):
-    with open(nombre_archivo, 'w') as archivo_json:
-        json.dump(resultados, archivo_json, indent=4)
-"""
+
 def procesar_carpeta_principal(carpeta_principal, nombre_archivo_json):
     resultados = []
     hilos = []
